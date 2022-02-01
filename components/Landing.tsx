7e9b4a60542f7bf1df4a1ac5b0ecaa9e0a8f628e
andykhoi <akitran@ucdavis.edu>
2022-02-01 14:21:00 -0800

    revert back to real twitter handle

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -43,7 +43,7 @@ export const Landing: FC<LandingProps> = ({
 				<div className="TaikiSocials">
 					<h2>Stay Updated</h2>
 					<a
-						href="https://twitter.com/twitter"
+						href="https://twitter.com/taikifriends"
 						target="_blank"
 						rel="noreferrer"
 					>
